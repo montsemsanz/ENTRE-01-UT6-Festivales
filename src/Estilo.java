d48e1275c1df0c639744d3d92df20be5f45610b7
amonrealidam1 <amonrealidam1@gmail.com>
2022-02-17 20:57:09 +0100

    Simplificada clase Estilos

--- src/Estilo.java
+++ src/Estilo.java
@@ -1,19 +1,4 @@
 public enum Estilo {
     HIPHOP, INDIE, POP, ROCK, FUSION, RAP, ELECTRONICA, PUNK, BLUES;
-
-    public String toString(){
-        switch (this){
-            case HIPHOP: return "HIPHOP";
-            case INDIE: return "INDIE";
-            case POP: return "POP";
-            case ROCK: return "ROCK";
-            case FUSION: return "FUSION";
-            case RAP: return "RAP";
-            case ELECTRONICA: return "ELECTRONICA";
-            case PUNK: return "PUNK";
-            case BLUES: return "BLUES";
-        }
-        return null;
-    }
 }
 
