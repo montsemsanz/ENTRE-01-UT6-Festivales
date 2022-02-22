5dc1eeb6e0c76bc591d372fe54991bf08bf984be
iciaadam1 <iciaadam1@gmail.com>
2022-02-22 19:30:56 +0100

    Añadido addFestival

--- src/AgendaFestivales.java
+++ src/AgendaFestivales.java
@@ -1,9 +1,6 @@
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
+import java.time.LocalDate;
+import java.util.*;
 
 
 /**
@@ -22,33 +19,37 @@ import java.util.TreeMap;
  */
 public class AgendaFestivales {
     private TreeMap<Mes, ArrayList<Festival>> agenda;
-    
+
     public AgendaFestivales() {
         this.agenda = new TreeMap<>();
     }
 
     /**
      * añade un nuevo festival a la agenda
-     *
+     * <p>
      * Si la clave (el mes en el que se celebra el festival)
      * no existe en la agenda se creará una nueva entrada
      * con dicha clave y la colección formada por ese único festival
-     *
+     * <p>
      * Si la clave (el mes) ya existe se añade el nuevo festival
      * a la lista de festivales que ya existe ese ms
      * insertándolo de forma que quede ordenado por nombre de festival.
      * Para este segundo caso usa el método de ayuda
      * obtenerPosicionDeInsercion()
-     *
      */
     public void addFestival(Festival festival) {
-        //TODO
-        
-        
+        if (!agenda.containsKey(festival.getFechaInicio())) {
+            ArrayList<Festival> festivales = new ArrayList<>();
+            festivales.add(festival);
+            agenda.put(festival.getMes(), festivales);
+        } else {
+            agenda.get(festival.getMes()).add(festival);
+        }
+
+
     }
 
     /**
-     *
      * @param festivales una lista de festivales
      * @param festival
      * @return la posición en la que debería ir el nuevo festival
@@ -56,34 +57,28 @@ public class AgendaFestivales {
      */
     private int obtenerPosicionDeInsercion(ArrayList<Festival> festivales,
                                            Festival festival) {
-       //TODO
-        
-        return 0;
-        
+
     }
 
     /**
      * Representación textual del festival
      * De forma eficiente
-     *  Usa el conjunto de entradas par recorrer el map
+     * Usa el conjunto de entradas par recorrer el map
      */
     @Override
     public String toString() {
-        //TODO
-        
-        return null;
+        String str = "";
+        return str;
     }
 
     /**
-     *
      * @param mes el mes a considerar
      * @return la cantidad de festivales que hay en ese mes
      * Si el mes no existe se devuelve -1
      */
     public int festivalesEnMes(Mes mes) {
-       //TODO
-        
-        return 0;
+
+        return agenda.get(mes).size();
     }
 
     /**
@@ -92,16 +87,17 @@ public class AgendaFestivales {
      * Cada estilo que aparece en la agenda tiene asociada una colección
      * que es el conjunto de nombres de festivales que pertenecen a ese estilo
      * Importa el orden de los nombres en el conjunto
-     *
+     * <p>
      * Identifica el tipo exacto del valor de retorno
      */
-    public  Map   festivalesPorEstilo() {
-       //TODO
+    public TreeMap festivalesPorEstilo() {
 
-         
 
-        return null;
+        }
+
+        return agendaEstilos;
     }
+
 
     /**
      * Se cancelan todos los festivales organizados en alguno de los
@@ -109,13 +105,14 @@ public class AgendaFestivales {
      * concluidos o que no empezados no se tienen en cuenta
      * Hay que borrarlos de la agenda
      * Si el mes no existe se devuelve -1
-     *
+     * <p>
      * Si al borrar de un mes los festivales el mes queda con 0 festivales
      * se borra la entrada completa del map
      */
     public int festivalesPorEstilo(HashSet<String> lugares, Mes mes) {
-       //TODO
-        
-        return 0;
+
+
     }
 }
+
+
